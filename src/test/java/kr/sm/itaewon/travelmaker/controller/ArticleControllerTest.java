be204b9f90567e87ca9e3b200f266e5e77885631
이태윤 <tyleeg0901@gmail.com>
2019-08-05 12:08:21 +0900

    add API method for step A, change model class field name

--- src/test/java/kr/sm/itaewon/travelmaker/controller/ArticleControllerTest.java
+++ src/test/java/kr/sm/itaewon/travelmaker/controller/ArticleControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,4 +29,6 @@ public class ArticleControllerTest {
         mockMvc.perform(get("/article/getAll")).andExpect(status().isOk());
 
     }
+
+
 }
